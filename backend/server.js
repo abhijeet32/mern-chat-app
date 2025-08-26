dc2ddccf82996488a32efdcaf244f75620ac5264
abhijeet32 <kumarabhijeetips@gmail.com>
2025-08-27 00:34:55 +0530

    editing backend files

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,10 @@ const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
+const cors = require("cors")
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
